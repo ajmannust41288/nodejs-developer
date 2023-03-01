4e2bf3fc314b79d71a8aa8163be60eeb75ee217d
ajmannust41288 <124865655+ajmannust41288@users.noreply.github.com>
2023-03-01 18:41:02 +0500

    nodejsTimeoutfunction
    examples of ascynchrounous tasks

--- asychrounoustasks/asychronoustask.js
+++ asychrounoustasks/asychronoustask.js
@@ -16,13 +16,27 @@
 // }, 2000);
 // console.log(a+b);//10
 //====================================Use of Promise
-let a=10;
-let waitingdata=new Promise((resolve,reject)=>{
-    setTimeout(() => {
-        resolve(30);
-    }, 2000);
-});
-waitingdata.then((data)=>{
-    b=data;
-    console.log(a+b);//print 40 after 2second
-});
+// let a=10;
+// let waitingdata=new Promise((resolve,reject)=>{
+//     setTimeout(() => {
+//         resolve(30);
+//     }, 2000);
+// });
+// waitingdata.then((data)=>{
+//     b=data;
+//     console.log(a+b);//print 40 after 2second
+// });
+//===============================================use of Timeout()
+// console.log("starting up");
+// setTimeout(()=>{
+//     console.log(" 2 seconds here");
+// },2000);
+// setTimeout(()=>{
+// console.log("o seond here");
+// },0);
+// console.log("Finishing UP");
+//output are here
+// starting up
+// Finishing UP
+// o seond here
+//  2 seconds here
