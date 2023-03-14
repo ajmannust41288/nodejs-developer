30f9849f211a12df5124082cff3a7abb34be4220
ajmannust41288 <124865655+ajmannust41288@users.noreply.github.com>
2023-03-14 21:34:33 +0500

    APImethodMongoose
    uses Post,Get,Put and Delete method api using Moongoose

---
+++ ApiMethodsMongoose/index6.js
@@ -0,0 +1,13 @@
+ //post api moongoose
+ const express=require('express');
+ require('./config');
+ const Product=require('./product');
+ const app=express();
+ app.use(express.json());
+ app.post("/create",async (req,resp)=>{
+    let data= new Product(req.body);
+    let result= await data.save();
+    console.log(req.body);
+    resp.send(req.body);
+ });
+ app.listen(5000);
